607ca7fa90af3ecffe1ac824ea0bd850bfe23db0
Cristian Perez <cperezlecaros@gmail.com>
2025-01-11 15:19:13 -0700

    Implement react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,25 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand as={Link} to="/">
             <img src={logo} alt="proshop" />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
           <Nav className="ms-auto">
-            <Nav.Link href="/cart">
+            <Nav.Link as={Link} to="/cart">
               <FaShoppingCart />
               Cart
             </Nav.Link>
-            <Nav.Link href="/login">
+            <Nav.Link as={Link} to="/login">
               <FaUser />
               Sign In
             </Nav.Link>

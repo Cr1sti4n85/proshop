63246ddd2f6098081770fbeedd776196a3d04c63
Cristian Perez <cperezlecaros@gmail.com>
2025-01-11 16:08:48 -0700

    Create Product details page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,13 +8,15 @@ import {
 } from "react-router-dom";
 import "./assets/bootstrap.custom.css";
 import "./assets/index.css";
-import App from "./App.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
+import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
+      <Route path="/product/:id" element={<ProductScreen />}></Route>
     </Route>
   )
 );

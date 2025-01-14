323f42488db1813051cd462284e3ef1de5d116a9
Cristian Perez <cperezlecaros@gmail.com>
2025-01-13 17:05:30 -0700

    Implement error middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import { EnvConfiguration } from "./config/envConfig";
 import productRoutes from "./routes/product.routes";
+import { notFound, errorHandler } from "./middleware/error.middleware";
 import "./config/db";
 
 const PORT = EnvConfiguration().port;
@@ -11,6 +12,9 @@ const app: Application = express();
 app.use(cors());
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);

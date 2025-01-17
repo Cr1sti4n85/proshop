fada6277eac8afcfe8980958c58d232dac90912c
Cristian Perez <cperezlecaros@gmail.com>
2025-01-16 17:28:50 -0700

    Build addToCart reducer

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addDecimals } from "../helpers/addDecimals";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -7,7 +8,43 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((i) => i._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((i) =>
+          i._id === existItem._id ? item : i
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //Calculate shipping price (over 100 is free, otherwise $10)
+
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      //Calculate tax price
+
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+      //Calculate total price
+
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;

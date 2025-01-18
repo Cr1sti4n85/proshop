09b55fbd58f5b6e9e3226300e58950026bf6ca12
Cristian Perez <cperezlecaros@gmail.com>
2025-01-17 17:33:05 -0700

    Refactor cart slice

---
+++ frontend/src/helpers/updateCart.js
@@ -0,0 +1,25 @@
+import { addDecimals } from "./addDecimals";
+
+export const updateCart = (state) => {
+  //Calculate items price
+  state.itemsPrice = addDecimals(
+    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  //Calculate shipping price (over 100 is free, otherwise $10)
+
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  //Calculate tax price
+
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+  //Calculate total price
+
+  state.totalPrice = (
+    Number(state.itemsPrice) +
+    Number(state.shippingPrice) +
+    Number(state.taxPrice)
+  ).toFixed(2);
+
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
+};
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addDecimals } from "../helpers/addDecimals";
+import { updateCart } from "../helpers/updateCart";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
@@ -21,27 +21,8 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      //Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-      //Calculate shipping price (over 100 is free, otherwise $10)
 
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      //Calculate tax price
-
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-      //Calculate total price
-
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });

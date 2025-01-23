52ec78c63db6379b2cfde6d4f637521c06e9ea9b
Cristian Perez <cperezlecaros@gmail.com>
2025-01-23 15:02:29 -0700

    Create auth slice and usersApiSlice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    //mutation for post request
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;

58af5f79e1ea083c7c912ac05856f66b8797a069
Cristian Perez <cperezlecaros@gmail.com>
2025-02-08 14:07:00 -0700

    Update app.ts for production env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app: Application = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    credentials: true, // Permitir cookies en solicitudes cross-origin
+    credentials: true,
   })
 );
 app.use(express.json());
@@ -34,9 +34,21 @@ app.get("/api/config/paypal", (_req: Request, res: Response) => {
   return;
 });
 
-//Set __dirname to current directory
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../..", "/uploads")));
+
+if (EnvConfiguration().NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../..", "/frontend/dist")));
+
+  app.get("*", (_req, res: Response) =>
+    res.sendFile(
+      path.resolve(__dirname, "../..", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (_req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);

8419d9500c3cdadde97db8f15862436a67cbff92
Cristian Perez <cperezlecaros@gmail.com>
2025-02-13 16:33:02 -0700

    Update generateToken function

--- backend/src/helpers/generateToken.ts
+++ backend/src/helpers/generateToken.ts
@@ -10,7 +10,7 @@ export const generateToken = (res: Response, id: string) => {
   //Set jwt as http only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: false,
+    secure: EnvConfiguration().NODE_ENV === "development" ? false : true,
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
